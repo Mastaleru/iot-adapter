61821244eac64b355fd1e728d66e166e603634af
nliappas <67898132+nliappas@users.noreply.github.com>
2022-06-21 15:14:57 +0200

    operation to handle when DP gets updated

--- iot-adapter/strategies/operations/dpermission.js
+++ iot-adapter/strategies/operations/dpermission.js
@@ -1,7 +1,7 @@
 const commonServices = require("common-services")
-const { DPService} = commonServices;
-
+const { DPService, CommunicationService} = commonServices;
 const dpService = DPService.getDPService();
+
 
 function create_dp(message) {
     dpService.mount(message.sReadSSI, (err, mountedDPermission) => {
@@ -12,6 +12,29 @@ function create_dp(message) {
     });
 }
 
+
+function dp_updated(message) {
+
+    dpService.mount(message.dpUID, (err, mountedDPermission) => {
+        if (err) {
+            console.log(err);
+        }
+        let matches = mountedDPermission.matches; 
+        let match = matches.filter(m => m.study.uid === message.studyUID)[0];
+        let researcherDID = match.study.researcherDID;
+        let studyUID = match.study.uid;
+        let communicationService = CommunicationService.getCommunicationServiceInstance();
+        let data = {
+            operation: "add_participants_to_study",
+            participant: match.patient,
+            studyUID: studyUID
+        }
+        communicationService.sendMessage(researcherDID, data)
+    });
+}
+
+
 module.exports = {
     "create_dp": create_dp,
+    "dp_updated": dp_updated
 }
