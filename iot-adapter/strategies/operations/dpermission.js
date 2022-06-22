d53e3f668dc3ed838957cae592fa42a8a1e3fcc2
nliappas <67898132+nliappas@users.noreply.github.com>
2022-06-22 10:59:10 +0200

    seperate functions for add delete permissions to researcher

--- iot-adapter/strategies/operations/dpermission.js
+++ iot-adapter/strategies/operations/dpermission.js
@@ -13,7 +13,7 @@ function create_dp(message) {
 }
 
 
-function dp_updated(message) {
+function dp_updated_add(message) {
 
     dpService.mount(message.dpUID, (err, mountedDPermission) => {
         if (err) {
@@ -33,8 +33,29 @@ function dp_updated(message) {
     });
 }
 
+function dp_updated_remove(message) {
+
+    dpService.mount(message.dpUID, (err, mountedDPermission) => {
+        if (err) {
+            console.log(err);
+        }
+        let matches = mountedDPermission.matches; 
+        let match = matches.filter(m => m.study.uid === message.studyUID)[0];
+        let researcherDID = match.study.researcherDID;
+        let studyUID = match.study.uid;
+        let communicationService = CommunicationService.getCommunicationServiceInstance();
+        let data = {
+            operation: "remove_participants_from_study",
+            participant: match.patient,
+            studyUID: studyUID
+        }
+        communicationService.sendMessage(researcherDID, data)
+    });
+}
+
 
 module.exports = {
     "create_dp": create_dp,
-    "dp_updated": dp_updated
+    "dp_updated_add": dp_updated_add,
+    "dp_updated_remove": dp_updated_remove
 }
