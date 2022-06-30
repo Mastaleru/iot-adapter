c1d5ffd1e79f301a2dc2703540fd2df00c4af6d0
nliappas <67898132+nliappas@users.noreply.github.com>
2022-06-30 10:50:43 +0200

    create permissionedData DSU WIP

--- iot-adapter/strategies/operations/dpermission.js
+++ iot-adapter/strategies/operations/dpermission.js
@@ -1,8 +1,9 @@
 const commonServices = require("common-services")
-const { DPService, CommunicationService, StudiesService} = commonServices;
+const { DPService, CommunicationService, StudiesService, HealthDataService, PermissionedHealthDataService} = commonServices;
 const dpService = DPService.getDPService();
 const studiesService = new StudiesService();
-
+const healthDataService= new HealthDataService();
+const permissionedHealthDataService = new PermissionedHealthDataService();
 
 function create_dp(message) {
     dpService.mount(message.sReadSSI, (err, mountedDPermission) => {
@@ -28,6 +29,7 @@ function dp_updated_add(message) {
                 console.log(err);
             }
             let researcherDID = studyFullData.researcherDID;
+            let requestedDataType = studyFullData.data;
             let communicationService = CommunicationService.getCommunicationServiceInstance();
             let data = {
                 operation: "add_participants_to_study",
@@ -38,6 +40,59 @@ function dp_updated_add(message) {
             }
             communicationService.sendMessage(researcherDID, data);
             console.log("ADD: Updating the D Permission in the researcher SSAPP");
+            
+            // Send data sReadSSI back to researcher
+        
+            healthDataService.getAllObservations((err, allPatientsObservations)=>{
+                if (err){
+                    return console.log(err);
+                }
+
+                let collectedObservations = [];
+
+                allPatientsObservations.forEach(patientObservations => {
+                    patientObservations.forEach(observation => {
+                        let patientTPNumber = observation.subject.reference.slice(8);
+                        let observationDataType = observation.code.text;
+                        if (patientTPNumber === data.participant.patientTPNumber &&  observationDataType === requestedDataType) {
+                            collectedObservations.push(observation)
+
+                            permissionedHealthDataService.getAllObservations((err, data)=> {
+                                if (err) {
+                                    console.log(err);
+                                }
+                                console.log(data);
+                                console.log("permissioned data here");
+                            });
+                            //console.log("found")
+                            //console.log(patientObservations.uid)
+
+                            // healthDataService.updateObservation(observation, (err, data)=>{
+                            //     if (err) {
+                            //         console.log(err);
+                            //     }
+                            //     console.log(data);
+                            // })
+
+                            // path = '/health-data'
+                            // healthDataService.getSReadSSI(path, (err, data)=> {
+                            //     if (err){
+                            //         console.log(err);
+                            //     }
+                            //     console.log(data);
+                            //     healthDataService.mountObservation(data, (err, mountedData)=> {
+                            //         if (err){
+                            //             console.log(err);
+                            //         }
+                            //         console.log(mountedData);
+                            //     })
+                            // })
+                            //return false;
+                        }
+                    });
+                });
+                console.log(collectedObservations);
+            });
         })
     });
 }
