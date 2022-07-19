75fba4f2aea15fa8c88dd19c4187b352e9ec4890
nliappas <67898132+nliappas@users.noreply.github.com>
2022-07-19 16:35:43 +0200

    keep rejections in DP

--- iot-adapter/strategies/operations/dpermission.js
+++ iot-adapter/strategies/operations/dpermission.js
@@ -104,9 +104,39 @@ function dp_updated_remove(message) {
     });
 }
 
+function dp_updated_reject(message) {
+
+    dpService.mount(message.dpUID, (err, mountedDPermission) => {
+        if (err) {
+            console.log(err);
+        }
+        let matches = mountedDPermission.matches; 
+        let match = matches.filter(m => m.studyUID === message.studyUID)[0];
+        let studyUID = match.studyUID;
+        studiesService.getStudy(studyUID, (err, studyFullData)=>{
+            if (err) {
+                console.log(err);
+            }
+            let researcherDID = studyFullData.researcherDID;
+            let communicationService = CommunicationService.getCommunicationServiceInstance();
+            let data = {
+                operation: "reject_participants_from_study",
+                participant: match.patient,
+                studyUID: match.studyUID,
+                dpermissionRejectedDate: match.dpermissionRejectedDate,
+                dpermission: match.dpermission
+            }
+            communicationService.sendMessage(researcherDID, data);
+            console.log("REJECT: Updating the D Permission in the researcher SSAPP");
+        })
+    });
+}
+
+
 
 module.exports = {
     "create_dp": create_dp,
     "dp_updated_add": dp_updated_add,
-    "dp_updated_remove": dp_updated_remove
+    "dp_updated_remove": dp_updated_remove,
+    "dp_updated_reject": dp_updated_reject
 }
