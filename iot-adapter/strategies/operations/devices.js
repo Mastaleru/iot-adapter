d9e9aee34e66acfd7070224d5cebcbd1790b52a0
mastaleru <mastaleru.rafael@gmail.com>
2022-09-07 12:29:08 +0300

    cleanup useless/unreachable code

--- iot-adapter/strategies/operations/devices.js
+++ iot-adapter/strategies/operations/devices.js
@@ -38,84 +38,8 @@ function add_device(message){
     });
 }
 
-function list_device(message){
-    let flow = $$.flow.start(domainConfig.type);
-    flow.init(domainConfig);
-    flow.searchResources("Device", (error, result)=>{
-        if (error) {
-            console.log(error);
-        }
-        else console.log(result);
-    });
-    
-}
-function update_device(message){
-    deviceService.mountDevice(message.sReadSSI, (err, mountedDevice) => {
-        if (err){
-            console.log(err);
-        }
-        const entityId = mountedDevice.objectId;
-        console.log(mountedDevice);
-        let flow = $$.flow.start(domainConfig.type);
-        flow.init(domainConfig);
-        flow.updateResource("Device", entityId,  mountedDevice , (error, result) => {
-            if (error) {
-                console.log(error.status, error);
-            } else {
-                console.log(result);
-            }
-        });
 
-    });
-}
-
-function get_a_device(message){
-    deviceService.mountDevice(message.ssi, (err, mountedEntity) => {
-        if (err){
-            console.log(err);
-        }
-        let flow = $$.flow.start(domainConfig.type);
-        flow.init(domainConfig);
-        flow.getResourceById("Device", mountedEntity.objectId, (error, result)=>{
-            if (error) {
-                console.log(error);
-            }
-            else console.log(result);
-        });
-    });
-}
-
-function delete_device(message){
-    console.log("**************** Delete Device Function  ******************");
-    // console.log(message);
-    let flow = $$.flow.start(domainConfig.type);
-    flow.init(domainConfig);
-    flow.getDeviceByUID(message.uid, (error, deviceData)=>{
-        console.log(deviceData);
-        if (error) {
-            console.log(error);
-        }
-        else {
-            let test = deviceData.results;
-            let data = test[0];
-            console.log(data.objectId)
-            flow.deleteResource("Device",data.objectId, (error, result)=>{
-                if (error) {
-                    console.log(error);
-                }
-                else console.log(result);
-            });
-        }
-    });
-    
-
-    
-}
 
 module.exports = {
     "add_device": add_device,
-    "list_device":list_device,
-    "update_device":update_device,
-    "get_a_device":get_a_device,
-    "remove_device":delete_device
 }
