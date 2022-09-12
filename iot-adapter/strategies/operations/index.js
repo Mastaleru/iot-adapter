6e0d168afca739bdce43fbb61a085a6f6d785a1f
nliappas <67898132+nliappas@users.noreply.github.com>
2022-09-12 18:53:33 +0200

    remove unused code

--- iot-adapter/strategies/operations/index.js
+++ iot-adapter/strategies/operations/index.js
@@ -1,4 +1,3 @@
-const evidences = require("./evidences.js");
 const devices = require("./devices.js");
 const deviceAssignation = require("./deviceAssignation.js");
 const deAssignDevice = require("./deAssignDevice.js");
@@ -6,7 +5,6 @@ const dpermission = require("./dpermission.js");
 const studies = require("./studies");
 
 module.exports = {
-    ...evidences,
     ...devices,
     ...deviceAssignation,
     ...deAssignDevice,
