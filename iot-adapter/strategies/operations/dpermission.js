9ab2df5ea2f1179ff3ac901246c108c823fa2388
nliappas <67898132+nliappas@users.noreply.github.com>
2022-10-04 14:30:37 +0200

    generate anonymized data for the whole study when completed

--- iot-adapter/strategies/operations/dpermission.js
+++ iot-adapter/strategies/operations/dpermission.js
@@ -34,22 +34,49 @@ function dp_updated_add(message) {
             }
             let researcherDID = studyFullData.researcherDID;
             let requestedDataTypes = studyFullData.data;
-            
-            // Find avaiable Data and Create  Permissioned data DSU
-            healthDataService.getAllObservations((err, allPatientsObservations)=>{
-                if (err){
-                    return console.log(err);
-                }
-                let collectedObservations = [];
 
+            let PermissionState = {
+                operation: Constants.MESSAGES.RESEARCHER.ADD_PARTICIPANTS_TO_STUDY,
+                participant: match.patient,
+                studyUID: match.studyUID,
+                dpermissionStartSharingDate: match.dpermissionStartSharingDate,
+                dpermission: match.dpermission
+            }
+            let communicationService = CommunicationService.getCommunicationServiceInstance();
+            communicationService.sendMessage(researcherDID, PermissionState);
+            console.log("ADD: New permission sent to the researcher.");
+        })
+    });
+}
+
+function generate_anonymize_data(message){
+
+    let participants_withPermission = [];
+    let TPNumbers = [];
+    let collectedObservations = [];
+    researcherDID = message.senderIdentity
+
+    studiesService.getStudy(message.ssi, (err, study_info)=> {
+        if (err){
+            console.log(err);
+        }
+        if (study_info.participants) participants_withPermission = study_info.participants.filter(p => p.dpermission===true);
+        TPNumbers = participants_withPermission.map(p => p.participantInfo.patientTPNumber);
+        let requestedDataTypes = study_info.data
+
+        healthDataService.getAllObservations((err, allPatientsObservations)=>{
+            if (err){
+                return console.log(err);
+            }
+            participants_withPermission.forEach(tp => {
                 requestedDataTypes.forEach(datatype => {
                     allPatientsObservations.forEach(patientObservations => {
                         patientObservations.observations.forEach(observation => {
                             let patientTPNumber = observation.subject.reference.slice(8);
                             let observationDataType = observation.code.text;
-                            if (patientTPNumber === matchTPNumber &&  observationDataType === datatype) {
-                                observation.subject.reference = match.patient.patientDID;
-                                observation.studyUID = message.studyUID;
+                            if (patientTPNumber === tp.participantInfo.patientTPNumber &&  observationDataType === datatype) {
+                                observation.subject.reference = tp.participantInfo.patientDID;
+                                observation.studyUID = message.ssi;
                                 observation.objectId = "N/A";
                                 observation.sk = "N/A";
                                 observation.identifier = "N/A";
@@ -58,27 +85,23 @@ function dp_updated_add(message) {
                         });
                     });
                 });
-
-                permissionedHealthDataService.saveObservation(collectedObservations, (err, savedData)=> {
-                    if (err){
-                        console.log(err);
-                    }
-                    let PermissionState = {
-                        operation: Constants.MESSAGES.RESEARCHER.ADD_PARTICIPANTS_TO_STUDY,
-                        participant: match.patient,
-                        studyUID: match.studyUID,
-                        dpermissionStartSharingDate: match.dpermissionStartSharingDate,
-                        dpermission: match.dpermission,
-                        permissionedDataDSUSSI: savedData.sReadSSI
-                    }
-                    let communicationService = CommunicationService.getCommunicationServiceInstance();
-                    communicationService.sendMessage(researcherDID, PermissionState);
-                    console.log(savedData);
-                    console.log("Generated %d anonymized observations for this participant.", savedData.length);
-                    console.log("ADD: New permission and permissioned data DSU to the researcher");
-                });
             });
-        })
+
+            permissionedHealthDataService.saveObservation(collectedObservations, (err, savedData)=> {
+                if (err){
+                    console.log(err);
+                }
+                let state = {
+                    operation: Constants.MESSAGES.RESEARCHER.ANONYMIZED_DATA,
+                    permissionedDataDSUSSI: savedData.sReadSSI
+                }
+                let communicationService = CommunicationService.getCommunicationServiceInstance();
+                communicationService.sendMessage(researcherDID, state);
+                console.log("Generated %d anonymized observations for this study.", savedData.length);
+                console.log("Total %d patients with permission for this study.", participants_withPermission.length);
+            });
+
+        });
     });
 }
 
@@ -144,5 +167,6 @@ module.exports = {
     "create_dp": create_dp,
     "dp_updated_add": dp_updated_add,
     "dp_updated_remove": dp_updated_remove,
-    "dp_updated_reject": dp_updated_reject
+    "dp_updated_reject": dp_updated_reject,
+    "generate_anonymize_data": generate_anonymize_data
 }
